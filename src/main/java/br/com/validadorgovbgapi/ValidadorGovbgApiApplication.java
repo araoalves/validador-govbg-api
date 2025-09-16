818f92a731e1dbf6e4d3bef8d067d54767468209
Arão Farias <arao.farias@sefaz.ma.gov.br>
2025-09-16 16:52:06 -0300

    Iniciando projeto

---
+++ src/main/java/br/com/validadorgovbgapi/ValidadorGovbgApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.validadorgovbgapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidadorGovbgApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValidadorGovbgApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/validadorgovbgapi/ValidadorGovbgApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.validadorgovbgapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidadorGovbgApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
